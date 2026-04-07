e525ad0388b647843c558312624fc352a914ddd7
Anjali Shyju <anjalishyju12@gmail.com>
2026-04-07 14:50:17 +0530

    fix superuser script

--- create_superuser.py
+++ create_superuser.py
@@ -1,13 +1,22 @@
+import os
+import django
+
+# ✅ SET YOUR PROJECT NAME HERE
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tyche_web.settings')
+
+# ✅ INITIALIZE DJANGO
+django.setup()
+
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 if not User.objects.filter(username="admin").exists():
     User.objects.create_superuser(
-        username="tyche_User",
-        email="anjalishyju12gmail.com",
-        password="tychepassword"
+        username="admin",
+        email="admin@gmail.com",
+        password="admin123"
     )
-    print("Superuser created")
+    print("✅ Superuser created")
 else:
-    print("Superuser already exists")
+    print("⚠️ Superuser already exists")
